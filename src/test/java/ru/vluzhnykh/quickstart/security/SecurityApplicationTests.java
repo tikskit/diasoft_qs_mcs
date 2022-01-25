04683a81c4e7983eb2aa576d925805354662e5ba
tikskit <tikskit@gmail.com>
2022-01-25 14:35:44 +0700

    Init commit

---
+++ src/test/java/ru/vluzhnykh/quickstart/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vluzhnykh.quickstart.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
