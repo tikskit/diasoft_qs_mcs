ad5573993d6a522d8f36d48cbcc6c4e08ab9a8c1
tikskit <tikskit@gmail.com>
2022-01-25 16:59:37 +0700

    подчищен импорт

--- src/main/java/ru/vluzhnykh/quickstart/security/security/SecurityConfiguration.java
+++ src/main/java/ru/vluzhnykh/quickstart/security/security/SecurityConfiguration.java
@@ -10,10 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.security.SecureRandom;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
