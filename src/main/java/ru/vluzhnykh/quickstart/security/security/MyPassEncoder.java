a4765694512a195a1b679ad14d287250bebac23c
tikskit <tikskit@gmail.com>
2022-02-09 10:23:06 +0700

    Добавлен комментарий для метода кодирования пароля

--- src/main/java/ru/vluzhnykh/quickstart/security/security/MyPassEncoder.java
+++ src/main/java/ru/vluzhnykh/quickstart/security/security/MyPassEncoder.java
@@ -5,6 +5,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class MyPassEncoder implements PasswordEncoder {
     @Override
     public String encode(CharSequence rawPassword) {
+        // Не будем сейчас возиться с зашифрованным паролем, храним его в открытом виде
         return rawPassword.toString();
     }
 
