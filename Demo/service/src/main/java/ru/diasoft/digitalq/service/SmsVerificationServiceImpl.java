67d1abdd94ea990d70f79869bca0e6ad9018c3a4
vluzhnykh <vluzhnykh@diasoft.ru>
2022-03-27 14:24:51 +0700

    Генерация кода для модуля DQ Hackathon Tutorial

---
+++ Demo/service/src/main/java/ru/diasoft/digitalq/service/SmsVerificationServiceImpl.java
@@ -0,0 +1,18 @@
+package ru.diasoft.digitalq.service;
+
+import ru.diasoft.digitalq.domain.SmsVerificationCheckRequest;
+import ru.diasoft.digitalq.domain.SmsVerificationCheckResponse;
+import ru.diasoft.digitalq.domain.SmsVerificationPostRequest;
+import ru.diasoft.digitalq.domain.SmsVerificationPostResponse;
+
+public class SmsVerificationServiceImpl implements SmsVerificationService {
+    @Override
+    public SmsVerificationCheckResponse dsSmsVerificationCheck(SmsVerificationCheckRequest smsVerificationCheckRequest) {
+        return null;
+    }
+
+    @Override
+    public SmsVerificationPostResponse dsSmsVerificationCreate(SmsVerificationPostRequest smsVerificationPostRequest) {
+        return null;
+    }
+}
